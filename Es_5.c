68563c500dcc5050c6956248142e36024b42f351
FedericoGiraudo <71812329+FedericoGiraudo@users.noreply.github.com>
2020-11-16 16:29:56 +0100

    Add files via upload

--- Es_5.c
+++ Es_5.c
@@ -3,7 +3,6 @@
 #include<string.h>
 #include<time.h>
 #define max 200
-#define NSONG 2000
 #define N 10
 
 
@@ -18,9 +17,15 @@ void Casuale(Canzone* playlist);
 
 int main() {
   FILE *file1;
+  int nrighe,c;
+  file1=fopen("esercizio.csv", "r");   // conta righe
+  for (c = getc(file1); c != EOF; c = getc(file1)){
+        if (c == '\n')
+         nrighe = nrighe + 1;
+        }
 
-Canzone playlist[NSONG];
- 
+Canzone *playlist ;
+playlist= (Canzone *)malloc(nrighe*sizeof(Canzone));
   
     printf("Apro il file...\n");
     printf("\n");
@@ -29,41 +34,41 @@ Canzone playlist[NSONG];
      if( file1==NULL ) {
         perror("Errore in apertura del file\n");
         exit(1);
-  } 
-  else{
-  Memorizzazione(playlist,file1);
-  printf("\n................................................\n");
-  Casuale(playlist);
-  }
+      } 
+         else{
+           Memorizzazione(playlist,file1);
+           printf("\n................................................\n");
+           Casuale(playlist);
+          }
    fclose(file1);
+   free(playlist);
 
   return 0;
 }
- 
 
- void Memorizzazione(Canzone* playlist, FILE* file1){
-  char riga[max];
-  char* field;
-  int counter=0;
-  while(fgets(riga, max, file1)) {
+  void Memorizzazione(Canzone* playlist, FILE* file1){
+   char riga[max];
+   char* field;
+   int counter=0;
+   while(fgets(riga, max, file1)) {
 
     field=strtok(riga,",");
-    (playlist+counter).numero =atoi(field);
+    (playlist+counter)->numero =atoi(field);
 
     field=strtok(NULL,",");
-    strcpy(playlist+counter).titolo,field);
+    strcpy((playlist+counter)->titolo,field);
 
     field=strtok(NULL,",");
-    strcpy(playlist+counter).artista,field);
+    strcpy((playlist+counter)->artista,field);
     
 
-    printf("numero: %d\n",(playlist+counter).numero);
-    printf("titolo: %s\n",(playlist+counter).titolo);
-    printf("artista: %s\n",(playlist+counter).artista);
+    printf("numero: %d\n",(playlist+counter)->numero);
+    printf("titolo: %s\n",(playlist+counter)->titolo);
+    printf("artista: %s\n",(playlist+counter)->artista);
 
     counter++;
+   }
   }
- }
 
 
 void Casuale(Canzone* playlist){
@@ -73,18 +78,15 @@ int num[N];
  	srand(time(0)); //inizializza il generatore sull'ora attuale dell'elaboratore time(0)
   
  	for(i=0;i<N;i++) {
-    	     (num+i)=rand()%10;
+    	     num[i] = rand()%10;
     	     for(j=0;j<i;j++) {
        		if((num+i)==(num+j)) {
             	   i--;
             	   break;
             }
          }
+         printf("numero: %d\n",(playlist+ *(num+i))->numero);
+         printf("titolo: %s\n",(playlist+ *(num+i))->titolo);
+         printf("artista: %s\n",(playlist+ *(num+i))->artista);
        }
-  
- 	for(i=0;i<N;i++){
-           printf("numero: %d\n",(playlist+(num+i)).numero);
-           printf("titolo: %s\n",(playlist+(num+i)).titolo);
-           printf("artista: %s\n",(playlist+(num+i)).artista);
-           }
 }  	   
